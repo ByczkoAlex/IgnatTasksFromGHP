bb35322bde1fefe6bd4a316ed6d4d05866ac0248
Alex <byczko2012@mail.ru>
2020-09-16 19:02:12 +0300

    Updates

---
+++ precache-manifest.08d18f5e7455d0b14eaaa913c7a23d9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244c698c10a36d43739b5094bfa3048f",
+    "url": "/IgnatTasksFromGHP/index.html"
+  },
+  {
+    "revision": "154722a6e0fa9c449fb7",
+    "url": "/IgnatTasksFromGHP/static/css/main.a5aa144e.chunk.css"
+  },
+  {
+    "revision": "bca0526635a48f123770",
+    "url": "/IgnatTasksFromGHP/static/js/2.6b541731.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/IgnatTasksFromGHP/static/js/2.6b541731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "154722a6e0fa9c449fb7",
+    "url": "/IgnatTasksFromGHP/static/js/main.c945fa11.chunk.js"
+  },
+  {
+    "revision": "9b84fad3b37fbb0fff27",
+    "url": "/IgnatTasksFromGHP/static/js/runtime-main.67862445.js"
+  }
+]);
